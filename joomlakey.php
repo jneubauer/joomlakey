09395054ecbec0119f31ece08c05cc43936d41b6
jneubauer <jon@jonneubauer.com>
2015-08-10 10:49:35 -0400

    restricting current shortcuts to backend

--- joomlakey.php
+++ joomlakey.php
@@ -6,6 +6,12 @@ class PlgSystemJoomlakey extends JPlugin
 {
 	function onBeforeRender ()
 	{
+		//only going to run these in the backend for now
+		//todo: figure out a way to implement separate frontend shortcuts for usability
+		$app = JFactory::getApplication();
+		if ($app->isSite()) {
+			return;
+		}
 		$document = & JFactory::getDocument();
 		$document->addScript(JURI::root().'plugins/system/joomlakey/mousetrap.js' );
 		$document->addScript(JURI::root().'plugins/system/joomlakey/joomlakey.js' );
